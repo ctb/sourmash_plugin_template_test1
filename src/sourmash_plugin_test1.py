be0e2dd04fad1dccdf1c4c944f9c209d484ca523
C. Titus Brown <titus@idyll.org>
2023-01-05 10:43:16 -0800

    example of how to modify template

--- src/sourmash_plugin_test1.py
+++ src/sourmash_plugin_test1.py
@@ -1,12 +1,8 @@
 "Read/write sketches via plugin."
-
-# TODO:
-# * add support for other moltypes
-
 import sourmash
 
 from sourmash.index import LinearIndex
-from sourmash.logging import debug_literal
+from sourmash.logging import debug_literal, notify
 
 from sourmash.save_load import (Base_SaveSignaturesToLocation,
                                 _get_signatures_from_rust)
@@ -19,10 +15,12 @@ from sourmash.save_load import (Base_SaveSignaturesToLocation,
 #
 
 def load_sketches(location, *args, **kwargs):
-    if location and location.endswith('.xyz'):
+    if location and location.endswith('.test1.sig'):
         # ... add your code here ...
-        # return LinearIndex(siglist)
-        pass
+        notify(f'*** test1 plugin is at work! Reading from {location}')
+        with open(location, 'rb') as fp:
+            siglist = list(sourmash.load_signatures(location))
+        return LinearIndex(siglist, location)
 
 load_sketches.priority = 5
 
@@ -31,7 +29,7 @@ load_sketches.priority = 5
 # save_to plugin - supports output to .xyz
 #
 
-class SaveSignatures_XYZ(Base_SaveSignaturesToLocation):
+class SaveSignatures_Test1(Base_SaveSignaturesToLocation):
     "Save signatures to a location."
     def __init__(self, location):
         super().__init__(location)
@@ -41,17 +39,20 @@ class SaveSignatures_XYZ(Base_SaveSignaturesToLocation):
     def matches(self, location):
         # match anything that is not None or ""
         if location:
-            return location.endswith('.xyz')
+            notify(f"*** test1 plugin matches {location}! using!")
+            return location.endswith('.test1.sig')
 
     def __repr__(self):
-        return f"SaveSignatures_AvroFile('{self.location}')"
+        return f"SaveSignatures_Test1('{self.location}')"
 
     def open(self):
         pass
 
     def close(self):
         # actually do the writing...
-        pass
+        with open(self.location, 'wb') as fp:
+            notify("*** test1 plugin is writing!")
+            sourmash.save_signatures(self.keep, fp)
 
     def add(self, ss):
         super().add(ss)
